b3e63e882e19a6a1dbb78ebfc6bbec96a8b41fa8
sricharan8462 <sricharan8462@yahoo.com>
2025-02-09 18:56:47 -0500

    added reset button

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   int _counter = 0;
-  bool _showFirstImage = true;
+  int _imageIndex = 0; 
   late AnimationController _controller;
   late Animation<double> _fadeAnimation;
+
+  final List<String> _images = [
+    "https://upload.wikimedia.org/wikipedia/en/1/13/One_Piece_Anime_Logo_International.png", // First Image
+    "https://upload.wikimedia.org/wikipedia/en/a/a4/Roronoa_Zoro.jpg", // Second Image
+    "https://upload.wikimedia.org/wikipedia/en/a/aa/Sanji_%28One_Piece%29.jpg?20211208102220" // Third Image
+  ];
 
   @override
   void initState() {
@@ -55,7 +61,14 @@ class _MyHomePageState extends State<MyHomePage>
   void _toggleImage() {
     _controller.forward(from: 0.0);
     setState(() {
-      _showFirstImage = !_showFirstImage;
+      _imageIndex = (_imageIndex + 1) % _images.length; // Cycles through 3 images
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _counter = 0;
+      _imageIndex = 0; // Resets to the first image
     });
   }
 
@@ -79,16 +92,31 @@ class _MyHomePageState extends State<MyHomePage>
             FadeTransition(
               opacity: _fadeAnimation,
               child: Image.network(
-                _showFirstImage
-                    ? "https://upload.wikimedia.org/wikipedia/en/1/13/One_Piece_Anime_Logo_International.png"
-                    : "https://upload.wikimedia.org/wikipedia/en/a/a4/Roronoa_Zoro.jpg",
+                _images[_imageIndex],
                 height: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return const Center(child: CircularProgressIndicator());
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return const Icon(Icons.error, size: 50, color: Colors.red);
+                },
               ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _toggleImage,
               child: const Text("Toggle Image"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _reset,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 252, 252, 252),
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                textStyle: const TextStyle(fontSize: 18),
+              ),
+              child: const Text("Reset"),
             ),
           ],
         ),
